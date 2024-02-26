7f188bd388ade2571911c93e1f7aed455ece6667
markyangcc <mmdou3@163.com>
2024-02-26 19:53:14 +0800

    pgtbl/vmprint: print pagetable when booting init process

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,29 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void pte_print_walk(pagetable_t pagetable, int level) {
+	// there are 2^9 = 512 PTEs in a page table.
+	for (int i = 0; i < 512; i++) {
+		pte_t pte = pagetable[i];
+		if (!(pte & PTE_V)) {
+			continue;
+		}
+
+		for (int i = 0; i <= level; i++) {
+			printf("..");
+		}
+		printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+		if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+			// this PTE points to a lower-level page table.
+			uint64 child = PTE2PA(pte);
+			pte_print_walk((pagetable_t)child, level + 1);
+		}
+	}
+}
+
+void vmprint(pagetable_t pagetable) {
+	printf("page table %p\n", pagetable);
+	pte_print_walk(pagetable, 0);
+}
