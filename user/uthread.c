f6821c175c0038c398e88974d0e1ebb21edf109f
markyangcc <mmdou3@163.com>
2024-06-23 14:43:06 +0800

    thread: impl uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,9 +10,31 @@
 #define STACK_SIZE 8192
 #define MAX_THREAD 4
 
+// Saved registers for uthread context switches.
+// Copy from kernel/proc.h
+struct context {
+  uint64 ra; /* return address */
+  uint64 sp; /* stack pointer */
+
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
 struct thread {
   char stack[STACK_SIZE]; /* the thread's stack */
   int state;              /* FREE, RUNNING, RUNNABLE */
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -55,10 +77,7 @@ thread_schedule(void)
     next_thread->state = RUNNING;
     t = current_thread;
     current_thread = next_thread;
-    /* YOUR CODE HERE
-     * Invoke thread_switch to switch from t to next_thread:
-     * thread_switch(??, ??);
-     */
+    thread_switch((uint64)&t->ctx, (uint64)&current_thread->ctx);
   }
   else
     next_thread = 0;
@@ -74,7 +93,8 @@ thread_create(void (*func)())
       break;
   }
   t->state = RUNNABLE;
-  // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;
+  t->ctx.sp = (uint64)&t->stack[STACK_SIZE - 1];
 }
 
 void
